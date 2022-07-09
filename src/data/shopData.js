087afdb37162d5c55e1f74b211987bc36d04381a
ErnestoCisnero <miguelernestocisnero@gmail.com>
2022-07-09 10:37:48 -0400

    adding data file

---
+++ src/data/shopData.js
@@ -0,0 +1,12 @@
+// PLCs, Modules, Development boards
+
+export const shopData = [
+    {
+        id:1,
+        category: "PLC",
+        manufacture: "Schneider Electric",
+        type: "M-221",// M241, M340, M580
+        quantity:20,
+        price:300,
+    }
+]
